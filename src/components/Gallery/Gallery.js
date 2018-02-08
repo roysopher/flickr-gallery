83ffc2f39737392430af52704dbbe8efeecd302f
Guy Sopher <guyso@Super-Mac-2.local>
2018-02-08 18:01:18 +0200

    minors

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -18,10 +18,6 @@ class Gallery extends React.Component {
     };
   }
 
-  componentDidMount() {
-    this.getImages(this.props.tag);
-  }
-
   getImages(tag) {
     const getImagesUrl = `services/rest/?method=flickr.photos.search&api_key=522c1f9009ca3609bcbaf08545f067ad&tags=${tag}&tag_mode=any&per_page=100&format=json&nojsoncallback=1`;
     const baseUrl = `https://api.flickr.com/`;
@@ -42,6 +38,10 @@ class Gallery extends React.Component {
           this.setState({images: res.photos.photo});
         }
       });
+  }
+
+  componentDidMount() {
+    this.getImages(this.props.tag);
   }
 
   componentWillReceiveProps(props) {
